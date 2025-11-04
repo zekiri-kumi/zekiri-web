202b9c6f72f97ff02935c0d2814acfe8e4a53001
richi0085 <ricardoclavijo0085@gmail.com.com>
2025-11-04 11:45:36 -0400

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   vite: {
       plugins: [tailwindcss()],
 	},
